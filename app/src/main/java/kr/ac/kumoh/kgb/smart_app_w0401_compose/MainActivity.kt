33b7ae17340006f347d81df1d96166f4c422c157
agfalcon <agfalcon12@gmail.com>
2022-09-27 15:52:34 +0900

    2022.09.27 compose app practice Layout Study2!

--- app/src/main/java/kr/ac/kumoh/kgb/smart_app_w0401_compose/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/kgb/smart_app_w0401_compose/MainActivity.kt
@@ -1,20 +1,22 @@
 package kr.ac.kumoh.kgb.smart_app_w0401_compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.leanback.widget.Row
-import kotlinx.coroutines.NonDisposableHandle.parent
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.kgb.smart_app_w0401_compose.ui.theme.Smart_app_w0401_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,20 +25,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             Smart_app_w0401_ComposeTheme {
                 // A surface container using the 'background' color from the theme
-                MyApp()
+                MyApp({Counter()})
             }
         }
     }
 }
 
 @Composable
-fun MyApp() {
+fun MyApp(content: @Composable () -> Unit ) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
 //        Greeting("안녕하세요")
-        MyLinearLayout()
+        Column {
+            content()
+            content()
+            content()
+        }
+
     }
 }
 
@@ -51,21 +58,42 @@ fun MyLinearLayout() {
                 .background(Color.Cyan)
                 .padding(8.dp))
         }
-        Text(text = "어디서 본 겉 같은 예제", modifier =  Modifier
+        Text(text = "어디서 본 겉 같은 예제", modifier = Modifier
             .background(Color.Magenta)
             .padding(8.dp))
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Counter() {
+//    var count by remember{
+//        mutableStateOf(0)
+//    }
+    val (count, setCount) = remember{ mutableStateOf(0) }
+    Column (modifier = Modifier
+        .padding(8.dp),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        Text( text="$count", fontSize = 70.sp, modifier = Modifier.padding(12.dp))
+        Row {
+            Button(onClick = {
+                setCount(count+1)
+                Log.i("counter", count.toString())},
+                modifier = Modifier.weight(1f)) {
+                Text(text = "증가")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { setCount(count-1) }, modifier = Modifier.weight(1f)) {
+                Text(text = "감소")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Smart_app_w0401_ComposeTheme {
-        MyApp()
+        MyApp({Counter()})
     }
 }
